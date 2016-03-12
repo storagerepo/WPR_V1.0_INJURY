4bed027e833229275c1a03758e37e831e8abd388
thangaduraiarepo <thangaduraia@outlook.com>
2016-03-12 12:02:29 +0530

    Commit after Did the AWS S3 File Upload.

---
+++ src/main/java/com/deemsys/project/pdfcrashreport/AWSFileUpload.java
@@ -0,0 +1,71 @@
+package com.deemsys.project.pdfcrashreport;
+
+import java.io.File;
+
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.*;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+@Service
+public class AWSFileUpload {
+
+	public void uploadFileToAWSS3(String filePath,String fileName){
+		
+		try{
+		// credentials object identifying user for authentication
+		// user must have AWSConnector and AmazonS3FullAccess
+		AWSCredentials credentials = new BasicAWSCredentials(
+						"AKIAJVAKKD6LO7WI6OYQ", 
+						"uTVQWKzreLhPe7f+jF/sdTCATsg1QyD78Mz/NJF+");
+		String bucketName ="injurycrashreports";
+		
+		// create a client connection based on credentials
+		AmazonS3 s3client = new AmazonS3Client(credentials);
+		System.out.println("Create Connection..........................");
+				
+				/*// create bucket - name must be unique for all S3 users
+				String bucketName = "javatutorial-net-example-bucket";
+				s3client.createBucket(bucketName);
+				
+				// list buckets
+				for (Bucket bucket : s3client.listBuckets()) {
+					System.out.println(" - " + bucket.getName());
+				}
+				// create folder into bucket
+				String folderName = "testfolder";
+				createFolder(bucketName, folderName, s3client);
+				*/
+				
+		// upload file to folder and set it to public
+		s3client.putObject(new PutObjectRequest(bucketName, fileName, 
+						new File(filePath))
+						.withCannedAcl(CannedAccessControlList.PublicRead));
+		System.out.println("File uploaded..........................");
+		} catch (AmazonServiceException ase) {
+			        System.out.println("Caught an AmazonServiceException, which " +
+			        		"means your request made it " +
+			                "to Amazon S3, but was rejected with an error response" +
+			                " for some reason.");
+			        System.out.println("Error Message:    " + ase.getMessage());
+			        System.out.println("HTTP Status Code: " + ase.getStatusCode());
+			        System.out.println("AWS Error Code:   " + ase.getErrorCode());
+			        System.out.println("Error Type:       " + ase.getErrorType());
+			        System.out.println("Request ID:       " + ase.getRequestId());
+		} catch (AmazonClientException ace) {
+		        System.out.println("Caught an AmazonClientException, which " +
+		        		"means the client encountered " +
+		                "an internal error while trying to " +
+		                "communicate with S3, " +
+		                "such as not being able to access the network.");
+		        System.out.println("Error Message: " + ace.getMessage());
+    }
+		
+	}
+	
+}
