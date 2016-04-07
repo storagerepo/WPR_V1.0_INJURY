be190d68778f1fef245097b7e8b1051aea632153
vinorepo <vinodhinib@deemsysinc.net>
2016-04-07 17:02:22 +0530

    commit with fixed header alignment

---
+++ src/main/webapp/WEB-INF/resources/js/fixed-header.js
@@ -0,0 +1,44 @@
+setTimeout(function(){ 
+		var tablewidth = $('.persist-area').width()+2;
+		$('.floatingHeader').width(tablewidth);
+	}, 1000);
+
+
+    function UpdateTableHeaders() {
+       $(".persist-area").each(function() {
+       
+           var el             = $(this),
+               offset         = el.offset(),
+               scrollTop      = $(window).scrollTop(),
+               floatingHeader = $(".floatingHeader", this)
+           if ((scrollTop > offset.top-50) && (scrollTop < offset.top + el.height()-50)) {
+               floatingHeader.css({
+                "visibility": "visible"
+               });
+           } else {
+               floatingHeader.css({
+                "visibility": "hidden"
+               });      
+           };
+       });
+    }
+    
+    // DOM Ready      
+    $(function() {
+    
+       var clonedHeaderRow;
+    
+       $(".persist-area").each(function() {
+           clonedHeaderRow = $(".persist-header", this);
+           clonedHeaderRow
+             .before(clonedHeaderRow.clone())
+             .css("width", clonedHeaderRow.width())
+             .addClass("floatingHeader");
+             
+       });
+       
+       $(window)
+        .scroll(UpdateTableHeaders)
+        .trigger("scroll");
+       
+    });
